4750d52b387632b33f222f961a2035a3acc53f4f
Kostantinos Makaronas <makaronaskonstandinos@gmail.com>
2026-08-19 14:40:42 +0300

    refactor: add utils.js and removed unused oscillator from audio

--- src/audio.js
+++ src/audio.js
@@ -1,37 +1,3 @@
-class Audio {
-  frequency = 440;
-  duration = 0.2;
-  volume = 0.1;
-  constructor() {
-    this.audioctx = new AudioContext();
-  }
-
-  play() {
-    const oscillator = this.audioctx.createOscillator();
-    const gain = this.audioctx.createGain();
-
-    const now = this.audioctx.currentTime;
-
-    oscillator.type = "square";
-    oscillator.frequency.setValueAtTime(this.frequency, now);
-
-    gain.gain.setValueAtTime(this.volume, now);
-    gain.gain.exponentialRampToValueAtTime(0.001, now + this.duration);
-
-    oscillator.connect(gain);
-    gain.connect(this.audioctx.destination);
-
-    oscillator.start(now);
-    oscillator.stop(now + this.duration);
-  }
-
-  resume() {
-    if (this.audioctx.state === "suspended") {
-      this.audioctx.resume();
-    }
-  }
-}
-
 const CARD_DRAW_SOUND = [
   0.22,
   0.04,
@@ -48,3 +14,15 @@ const CARD_DRAW_SOUND = [
   0,
   0.08,
 ];
+
+audioReady = false;
+
+ensureAudioReady = () => {
+  if (audioReady) return;
+
+  if (zzfxX.state === "suspended") {
+    zzfxX.resume();
+  }
+
+  audioReady = zzfxX.state === "running";
+};
